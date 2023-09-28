2f770c4b0c9668b278805ae58b2d68357ae74fd1
s10015164 <ava.taylor258@gmail.com>
2023-09-28 10:30:41 -0500

    Initial Commit - Question1Screen, CorrectScreen, ErrorScreen

---
+++ main.py
@@ -0,0 +1,24 @@
+# Ava Taylor
+
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class LoginPageApp(App):
+    pass
+
+class LoginManager(ScreenManager):
+    pass
+
+class Question1Screen(Screen):
+    pass
+
+class CorrectScreen(Screen):
+    pass
+
+class ErrorScreen(Screen):
+    pass
+
+
+
+LoginPageApp().run()
