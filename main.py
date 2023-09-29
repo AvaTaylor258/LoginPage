b2c3211b3e772132121dd82f293759bb04414089
s10015164 <ava.taylor258@gmail.com>
2023-09-29 10:30:37 -0500

    2nd Commit - Linked screens, added TextInputScreen

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 # Ava Taylor
 
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+# load the kv file
+Builder.load_file("LoginPage.kv")
 
 class LoginPageApp(App):
-    pass
+    def build(self):
+        return QuizManager()
 
-class LoginManager(ScreenManager):
+class QuizManager(ScreenManager):
     pass
 
 class Question1Screen(Screen):
-    pass
+    def answer_question(self, bool):
+        if bool:
+            self.manager.current = "correct"
+        else:
+            self.manager.current = "error"
 
 class CorrectScreen(Screen):
-    pass
+    def answer_question(self):
+        self.manager.current = "text input"
 
 class ErrorScreen(Screen):
     pass
+
+class TextInputScreen(Screen):
+    def answer_question(self, thing):
+        self.manager.current = thing
 
 
 
